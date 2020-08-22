50ffacb2fe3ce9a4e5ca90a134989d3674dc2c5c
becauseoflife <1752196851@qq.com>
2020-08-23 00:43:57 +0800

    优化查看文章详情功能和网页的切换

--- article/urls.py
+++ article/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 
 import article.views
 
+# 正在部署的应用名称
+app_name = 'article'
+
 urlpatterns = [
-    path('article_list', article.views.article_list, name='article_list')
+    path('article_list', article.views.article_list, name='article_list'),
+    path('article_detail/<int:id>/', article.views.article_detail, name='article_detail')
 ]
--- article/views.py
+++ article/views.py
@@ -7,7 +7,9 @@ from django.http import HttpResponse
 # 导入数据模型 ArticlePost
 from .models import ArticlePost
 
+
 # 视图函数
+# 文章列表
 def article_list(request):
     # return HttpResponse('Hello World!');
     # 取出所有的文章
@@ -18,3 +20,15 @@ def article_list(request):
     }
     # render 函数：载入模板 返回context对象
     return render(request, 'article/list.html', context)
+
+
+# 文章详情
+def article_detail(request, id):
+    # 取出相应的文章
+    article = ArticlePost.objects.get(id=id)
+    # 需要传递给模板的对象
+    context = {
+        'article': article
+    }
+    # 载入模板，并返回 context对象
+    return render(request, 'article/detail.html', context)
