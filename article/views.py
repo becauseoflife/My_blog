d3e4cb696abcad58d9fc2db96ef64a815b10274e
becauseoflife <1752196851@qq.com>
2020-08-23 15:39:29 +0800

    引进了Markdown语法以及代码高亮扩展

--- article/views.py
+++ article/views.py
@@ -5,7 +5,10 @@ from django.shortcuts import render
 # 导入HTTPResponse 模块
 from django.http import HttpResponse
 # 导入数据模型 ArticlePost
+
 from .models import ArticlePost
+# 引入Markdown模块
+import markdown
 
 
 # 视图函数
@@ -26,9 +29,25 @@ def article_list(request):
 def article_detail(request, id):
     # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
+
+    # 将Markdown语法渲染成HTML样式
+    article.body_content = markdown.markdown(article.body_content,
+                                             extensions=[
+                                                 # 包含 缩写、表格等常用扩展
+                                                 'markdown.extensions.extra',
+                                                 # 语法高亮扩展
+                                                 'markdown.extensions.codehilite',
+                                                 # 目录扩展
+                                                 'markdown.extensions.toc',
+                                             ])
+
+    # print(article.body_content)
+
     # 需要传递给模板的对象
     context = {
         'article': article
     }
     # 载入模板，并返回 context对象
     return render(request, 'article/detail.html', context)
+
+#
