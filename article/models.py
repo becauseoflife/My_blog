286a968db5c1339babac2789404391d03c8823de
becauseoflife <1752196851@qq.com>
2020-09-02 11:16:32 +0800

    上传并处理文章的标题图

--- article/models.py
+++ article/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.db import models
 
 # Create your models here.
@@ -56,6 +57,9 @@ class ArticlePost(models.Model):
     # 文章标签
     tags = TaggableManager(blank=True)
 
+    # 文章标题图
+    avatar = models.ImageField(upload_to='article/%Y%m%d/', blank=True)
+
     # 内部类 class Meta 用于给 model 定义元数据
     class Meta:
         # ordering 指定模型返回的数据的排列顺序
@@ -70,6 +74,22 @@ class ArticlePost(models.Model):
     # 获取文章地址
     def get_absolute_url(self):
         return reverse('article:article_detail', args=[self.id])
+
+    # 保存时处理图片
+    def save(self, *args, **kwargs):
+        # 调用原有的save功能
+        article = super(ArticlePost, self).save(*args, **kwargs)
+
+        # 固定图片大小宽度
+        if self.avatar and not kwargs.get('update_fields'):
+            image = Image.open(self.avatar)
+            (x, y) = image.size
+            new_x = 400
+            new_y = int(new_x * (y / x))
+            resized_inmage = image.resize((new_x, new_y), Image.ANTIALIAS)
+            resized_inmage.save(self.avatar.path)
+
+        return article
 
 
 
